0f4a6cf238c3e74d01e11a0dccf420940e23ebf1
Pavel Zlatanov <pavel.zlatanov@gmail.com>
2015-01-17 19:17:27 +0200

    Lusca security middlewares added

--- config/express.js
+++ config/express.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var swig = require('swig');
+var lusca = require('lusca');
 
 module.exports = function (app, config, passport) {
     app.set('port', config.session);
@@ -13,21 +14,28 @@ module.exports = function (app, config, passport) {
 	
 	// Set views path and view engine
 	app.set('view engine', 'html');
-	
 	app.set('views', path.join(__dirname, './../server/views'));
     
 	app.set('view cache', false);
-	
 	swig.setDefaults({ cache: false });
 
     app.use(favicon());
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded());
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(cookieParser());
-    
+
     app.use(session(config.session));
     app.use(passport.initialize());
     app.use(passport.session());
+
+    // Lusca security configuration
+    app.use(lusca.csrf());
+    app.use(lusca.csp({ /* ... */}));
+    app.use(lusca.xframe('SAMEORIGIN'));
+    app.use(lusca.p3p('ABCDEF'));
+    app.use(lusca.hsts({ maxAge: 31536000 }));
+
+    app.use(lusca.xssProtection(true));
 
     app.use(require('less-middleware')(path.join(__dirname, 'public')));
     app.use('/', express.static(path.join(__dirname, 'public')));
