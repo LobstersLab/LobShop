48592281803053a48b8d82a48730f31518268f35
vidimitrov <vidimitrov94@gmail.com>
2015-01-21 22:02:09 +0200

    CSS and JavaScript assets included in app.locals to be included dynamically in the layout view. Swig default variable braces chaget to <%= %>

--- config/express.js
+++ config/express.js
@@ -8,16 +8,48 @@ var swig = require('swig');
 var lusca = require('lusca');
 
 module.exports = function (app, config, passport) {
+    //
+    app.locals.cssFiles = [
+        '/assets/css/style.css'
+    ];
+    app.locals.jsFiles = [
+        '/assets/bower_components/angular/angular.js',
+        '/assets/bower_components/angular-bootstrap/ui-bootstrap.js',
+        '/assets/bower_components/angular-route/angular-route.js',
+        '/assets/bower_components/angular-resource/angular-resource.js',
+        '/assets/bower_components/angular-cookies/angular-cookies.js',
+        '/assets/bower_components/angular-animate/angular-animate.js',
+        '/config.js',
+        '/application.js',
+        '/app/modules/core/module.js',
+        '/app/modules/catalog/module.js',
+        '/app/modules/shoppingCart/module.js',
+        '/app/modules/users/module.js',
+        '/app/shared/header/module.js',
+        '/app/shared/menu/module.js',
+        '/app/shared/product/module.js',
+        '/app/shared/search/module.js',
+        '/app/modules/core/configs/config.js',
+        '/app/modules/core/configs/routes.js',
+        '/app/modules/core/controllers/coreController.js',
+
+    ];
+
     app.set('port', config.session);
 
     app.engine('html', swig.renderFile);
 
     // Set views path and view engine
     app.set('view engine', 'html');
-    app.set('views', path.join(__dirname, './../server/views'));
+    app.set('views', path.join(config.rootPath, 'server/views'));
+
+    app.use(express.static(path.join(config.rootPath, 'public')));
 
     app.set('view cache', false);
-    swig.setDefaults({cache: false});
+    swig.setDefaults({
+        cache: false,
+        varControls: ['<%=', '%>']
+    });
 
     app.use(favicon());
     app.use(bodyParser.json());
@@ -35,10 +67,6 @@ module.exports = function (app, config, passport) {
         app.use(lusca.xframe('SAMEORIGIN'));
         app.use(lusca.p3p('ABCDEF'));
         app.use(lusca.hsts({maxAge: 31536000}));
-
         app.use(lusca.xssProtection(true));
     }
-
-    app.use(require('less-middleware')(path.join(__dirname, 'public')));
-    app.use('/', express.static(path.join(__dirname, 'public')));
 };
