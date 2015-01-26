e71df3bc49cf298fac1592211a4fba18796b2c3d
Veselin Dimitrov <vidimitrov94@gmail.com>
2015-01-26 22:10:35 +0200

    Morgan logger added

--- config/express.js
+++ config/express.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var favicon = require('static-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var swig = require('swig');
 var lusca = require('lusca');
+var morgan = require('morgan');
 
 module.exports = function (app, config, passport) {
-    //
+
     app.locals.cssFiles = [
         '/assets/css/bootstrap.css',
         '/assets/css/style.css'
@@ -45,28 +47,51 @@ module.exports = function (app, config, passport) {
 
     app.set('port', config.session);
 
-    app.engine('html', swig.renderFile);
-
     // Set views path and view engine
+    app.engine('html', swig.renderFile);
     app.set('view engine', 'html');
     app.set('views', path.join(config.rootPath, 'server/views'));
 
-    app.use(express.static(path.join(config.rootPath, 'public')));
-
+    // Swig template engine configurations
     app.set('view cache', false);
     swig.setDefaults({
         cache: false,
         varControls: ['<%=', '%>']
     });
 
+    // Setting static folder to serve
+    app.use(express.static(path.join(config.rootPath, 'public')));
+
+
     app.use(favicon());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(cookieParser());
 
+    // Configure session management
     app.use(session(config.session));
+
+    // Initialize passport
     app.use(passport.initialize());
     app.use(passport.session());
+
+    // Enable logger (morgan) and set where to save the logs
+    var LOGS_PATH = config.rootPath + '/logs';
+
+    if (!fs.existsSync(LOGS_PATH)) {
+        fs.mkdirSync(LOGS_PATH);
+    }
+
+    var accessLogStream = fs.createWriteStream(LOGS_PATH + '/access.log', {flags: 'a'});
+
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    }
+    else {
+        app.use(morgan('combined', {
+            stream: accessLogStream
+        }));
+    }
 
     // Lusca security configuration
     if (config.env == 'production') {
