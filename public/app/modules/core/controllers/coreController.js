39bc2898b5517af5a1ee640f39df2d373ec70a89
Veselin Dimitrov <vidimitrov94@gmail.com>
2015-02-03 22:09:52 +0200

    ng-repeat tests WIP

--- public/app/modules/core/controllers/coreController.js
+++ public/app/modules/core/controllers/coreController.js
@@ -1,8 +1,39 @@
 'use strict';
 
-angular.module('core').controller('CoreController', ['$scope', 'ProductsResource',
-    function coreController ($scope, ProductsResource) {
-        $scope.message = 'Hello AngularJS, we are Lobsters Lab!';
+angular.module('core').controller('CoreController', ['ProductsResource',
+    function coreController (ProductsResource) {
+        this.message = 'Hello AngularJS, we are Lobsters Lab!';
 
-        $scope.products = ProductsResource.getAll();
+        //this.products = ProductsResource.getAll();
+        this.products = [{
+            title: 'Circa #102',
+            description: 'Very good coin',
+            lot: 210,
+            price: 240000,
+            year: 1800
+        }, {
+            title: 'Circa #103',
+            description: 'Very good coin',
+            lot: 211,
+            price: 240000,
+            year: 1800
+        }, {
+            title: 'Circa #104',
+            description: 'Very good coin',
+            lot: 212,
+            price: 240000,
+            year: 1800
+        }, {
+            title: 'Circa #105',
+            description: 'Very good coin',
+            lot: 213,
+            price: 240000,
+            year: 1800
+        }, {
+            title: 'Circa #106',
+            description: 'Very good coin',
+            lot: 214,
+            price: 240000,
+            year: 1800
+        }];
     }])
