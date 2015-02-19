52dae2aecff244786cb7e1ed582fa6dee702bf2e
Veselin Dimitrov <vidimitrov94@gmail.com>
2015-02-19 13:01:37 +0200

    Orders controller and views added

--- public/app/modules/shoppingCart/controllers/orderController.js
+++ public/app/modules/shoppingCart/controllers/orderController.js
@@ -1,8 +1,30 @@
 'use strict';
 
 angular.module('shoppingCart')
-    .controller('OrderController', [
-        function OrderController () {
+    .controller('OrderController', ['$scope',
+        function OrderController ($scope) {
             var self = this;
+
+            self.user = {};
+
+            self.isEmpty = function (fieldName) {
+                var field;
+
+                if (!fieldName) {
+                    return false;
+                }
+                else if (self.user[fieldName]) {
+                    field = self.user[fieldName];
+                }
+                else {
+                    field = $scope.orderForm[fieldName].$viewValue;
+                }
+
+                if (!field || field.length === 0) {
+                    return true;
+                }
+
+                return false;
+            };
         }
     ]);
